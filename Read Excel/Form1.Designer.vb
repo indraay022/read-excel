eb1b6ecf3901703161da3b648488c895b61b2e0a
indraay022 <indraay022@gmail.com>
2024-02-19 21:32:38 +0700

    Add project files.

---
+++ Read Excel/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        DataGridView1 = New DataGridView()
+        btnBrowse = New Button()
+        Label1 = New Label()
+        txtFileName = New TextBox()
+        Label2 = New Label()
+        cboSheet = New ComboBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(12, 12)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(776, 312)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' btnBrowse
+        ' 
+        btnBrowse.Location = New Point(749, 330)
+        btnBrowse.Name = "btnBrowse"
+        btnBrowse.Size = New Size(39, 23)
+        btnBrowse.TabIndex = 1
+        btnBrowse.Text = "..."
+        btnBrowse.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 334)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(66, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "File Name :"
+        ' 
+        ' txtFileName
+        ' 
+        txtFileName.Location = New Point(84, 330)
+        txtFileName.Name = "txtFileName"
+        txtFileName.ReadOnly = True
+        txtFileName.Size = New Size(659, 23)
+        txtFileName.TabIndex = 3
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 372)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(42, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "Sheet :"
+        ' 
+        ' cboSheet
+        ' 
+        cboSheet.FormattingEnabled = True
+        cboSheet.Location = New Point(84, 369)
+        cboSheet.Name = "cboSheet"
+        cboSheet.Size = New Size(121, 23)
+        cboSheet.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(cboSheet)
+        Controls.Add(Label2)
+        Controls.Add(txtFileName)
+        Controls.Add(Label1)
+        Controls.Add(btnBrowse)
+        Controls.Add(DataGridView1)
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Read Excel"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtFileName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cboSheet As ComboBox
+
+End Class
---
+++ Read Excel/Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO
+Imports System.Text
+Imports ExcelDataReader
+
+Public Class Form1
+    Dim tables As DataTableCollection
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Using ofd = New OpenFileDialog With {.Filter = "Excel Workbook|*.xlsx"}
+            If ofd.ShowDialog = DialogResult.OK Then
+                txtFileName.Text = ofd.FileName
+
+                System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance)
+
+                Using stream = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read)
+                    Using reader = ExcelReaderFactory.CreateReader(stream)
+                        Dim result = reader.AsDataSet(New ExcelDataSetConfiguration() With {
+                                                                 .ConfigureDataTable = Function(__) New ExcelDataTableConfiguration With {
+                                                                 .UseHeaderRow = True}})
+                        tables = result.Tables
+                        cboSheet.Items.Clear()
+
+                        For Each table As DataTable In tables
+                            cboSheet.Items.Add(table.TableName)
+                        Next
+                    End Using
+                End Using
+            End If
+        End Using
+    End Sub
+
+    Private Sub cboSheet_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboSheet.SelectedIndexChanged
+        Dim dt As DataTable = tables(cboSheet.SelectedItem.ToString())
+        DataGridView1.DataSource = dt
+    End Sub
+End Class
